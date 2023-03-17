26228b2cce49ca7d42440044eae938adb6828948
NhatMinh0208 <minhkhicon2468@gmail.com>
2023-03-17 13:32:31 +0000

    Added wantedLevel var to players and updated DB schema

---
+++ sv_init.lua
@@ -0,0 +1,27 @@
+local plyMeta = FindMetaTable("Player")
+
+function plyMeta:setWantedLevel(level)
+    self:setDarkRPVar("wantedLevel", level)
+    DarkRP.storeWantedLevel(self, level)
+end
+
+--[[---------------------------------------------------------------------------
+Debug chat commands
+---------------------------------------------------------------------------]]
+DarkRP.defineChatCommand("setwantedlevel", function(ply, args)
+    local level = DarkRP.toInt(args) or 0
+    ply:setWantedLevel(level)
+    level = ply:getWantedLevel()
+
+    DarkRP.notify(ply, 2, 5, "Your wanted level has been set to: " .. tostring(level))
+
+    return ""
+end)
+
+DarkRP.defineChatCommand("getwantedlevel", function(ply, args)
+    level = ply:getWantedLevel()
+
+    DarkRP.notify(ply, 2, 5, "Your wanted level is: " .. tostring(level))
+
+    return ""
+end)
